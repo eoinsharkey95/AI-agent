2f58ae6c818bd01284edc64445378086dff4e0e0
eoinsharkey95 <eoinsharkey95@gmail.com>
2026-02-23 11:00:10 +0000

    addition of --verbose command line flag

--- main.py
+++ main.py
@@ -17,11 +17,11 @@ client = genai.Client(api_key=api_key)
 import argparse
 parser = argparse.ArgumentParser(description="Chatbot")
 parser.add_argument("user_prompt", type=str, help="User prompt")
+parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
 args = parser.parse_args()
 
 from google.genai import types
 messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
-
 
 def main():
     print("Hello from ai-agent!")
@@ -32,10 +32,14 @@ def main():
 
     if response.usage_metadata == None:
         raise RuntimeError("No usage metadata present - likely failed API request")
+    
     prompt_tokens = response.usage_metadata.prompt_token_count
     response_tokens = response.usage_metadata.candidates_token_count
-    print(f'Prompt tokens: {prompt_tokens}')
-    print(f'Response tokens: {response_tokens}')
+    
+    if args.verbose:
+        print(f'User prompt: {args.user_prompt}')
+        print(f'Prompt tokens: {prompt_tokens}')
+        print(f'Response tokens: {response_tokens}')
 
     print(response.text)
 
